9bcbb10dcf3b41e0ad4624a51216555ea505e820
Hari-repo <hariharanuk810@gmail.com>
2025-09-14 21:10:39 +0530

    User variables added

--- AI Project/app.py
+++ AI Project/app.py
@@ -2,9 +2,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
+name = "Hari"
+age = 39
+height = 5.9
+is_student = True 
+
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return f' Name : {name}, Age: {age}, Height: {height}, Is Student: {is_student}'
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+print(name, age, height, is_student)
