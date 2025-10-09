c3392f087f4e8b20fc64bc214e897563e8b444b5
Hari-repo <hariharanuk810@gmail.com>
2025-10-09 20:05:53 +0530

    Removed unwanted boilerplate codes generated by npm and vite

--- React99/swiggy/src/App.jsx
+++ React99/swiggy/src/App.jsx
@@ -17,19 +17,10 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hari Welcome to React Bootcamp</h1>
+      <h1>Welcome to React Bootcamp</h1>
       <Tamil />
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+      
     </>
   )
 }
