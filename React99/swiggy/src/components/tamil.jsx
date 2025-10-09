16ceca34a216f3b5dcb5914d3ac985a9b248f607
Hari-repo <hariharanuk810@gmail.com>
2025-10-09 19:58:46 +0530

    React 99 initial commit - created Tamil component

---
+++ React99/swiggy/src/components/tamil.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+const Tamil = () => {
+    return (
+        <div>
+            <h1>வணக்கம், இது ஒரு தமிழ் கூறு!</h1>
+            <p>இந்த கூறு React பயன்பாட்டில் பயன்படுத்தப்படுகிறது.</p>
+        </div>
+    );
+}
+
+export default Tamil
