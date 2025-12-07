3397a46ad258d6c9fb9348048747c29f043726f5
Hari-repo <hariharanuk810@gmail.com>
2025-12-07 11:28:38 +0530

    Initial commit for rest Sample

---
+++ WTS/rest.js
@@ -0,0 +1,6 @@
+const movies = ['Titanic','Inception','The Dark Knight','Reverant']
+const [firstMovie, ...leftOver]= movies;
+console.log('------------------------');
+console.log(firstMovie);
+console.log('------------------------');
+console.log(leftOver);
