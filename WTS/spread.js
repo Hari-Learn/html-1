8c0aaa394b8d025552d7aba82ca887fb87568392
Hari-repo <hariharanuk810@gmail.com>
2025-12-09 20:11:05 +0530

    Common js method added

---
+++ WTS/spread.js
@@ -0,0 +1,20 @@
+let fruits1 = ['apple', 'banana'];
+fruits1.push('orange');
+console.log(fruits); // ['apple', 'banana', 'orange']
+
+
+let fruits2 = ['apple', 'banana', 'orange'];
+let last = fruits2.pop();
+console.log(last);   // 'orange'
+console.log(fruits2); // ['apple', 'banana']
+
+
+let fruits3 = ['apple', 'banana', 'orange'];
+let first = fruits3.shift();
+console.log(first3);  // 'apple'
+console.log(fruits3); // ['banana', 'orange']
+
+
+let fruits = ['banana', 'orange'];
+fruits.unshift('apple');
+console.log(fruits); // ['apple', 'banana', 'orange']
