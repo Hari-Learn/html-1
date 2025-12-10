1bd5c4ba83158b0f83305a59ba57fb328ff94275
Hari-repo <hariharanuk810@gmail.com>
2025-12-10 22:01:04 +0530

    Tailwind config file created

---
+++ WTS/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"], // <-- This must point to your HTML files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
