6da11ad4eba6f7d6ac9f52bf3e16296483f31572
Hari-repo <hariharanuk810@gmail.com>
2025-12-16 00:21:28 +0530

    array destructruing completed

--- WTS/arrayDestruc.js
+++ WTS/arrayDestruc.js
@@ -13,3 +13,29 @@ const nums = [5]
 const [a=5,b=10] = nums
 console.log(a,b);
 
+//swapping variables
+
+let i = 10;
+let j = 20;
+
+[i,j] = [j,i]
+console.log(i,j);
+
+//using rest operator to collecting remaining elements
+
+const fruits = ["banana","apple","orange","kiwi"]
+
+const [firstFruit,...restofFruits] = fruits;
+console.log(firstFruit,restofFruits);
+
+//Nested array destructruing
+
+const numss = [1,[2,3],4];
+const[one,[two,three],four]=numss;
+console.log(one,two,three,four);
+
+//real world example
+
+const response = ["success",200,{data:"ok"}]
+const [status,code,realData] = response;
+console.log(status,code,realData);
