9c06bc3dc906b842aeb8629ff802bc435ff026d5
Hari-repo <hariharanuk810@gmail.com>
2025-12-15 10:54:15 +0530

    Renaming and default value in object destructruting ADDED

--- WTS/objDestrucruting.js
+++ WTS/objDestrucruting.js
@@ -11,8 +11,44 @@ const names = user.name;
 const ages = user.age;
 console.log(`User obj : ${names} and age is ${ages}`);
 
-//With obj destruc
+//With obj destruc(ES6)
 
 const {name, age} = user;
 console.log(`User obj : ${name} and age is ${age}`);
+
+
+//2.Basic Syntax
+//const {key} = object
+//variable name must match keys
+const person = {
+    firstName : "Hari",
+    lastName:"Haran"
+}
+
+const{firstName,lastName} = person;
+console.log(firstName,lastName);
+
+const{firstnames,lastnames} = person;
+console.log(firstnames,lastnames); //undefined undefined so key must match with variable name
+
+//Renaming variables
+const person1 = {
+     firstN : "Hari",
+    lastN:"Haran 1"
+}
+//const {fname:firstN,lname:lastN} = person1; // wrong way mistake 
+//syntax const { originalPropertyName: newVariableName } = object;
+const {firstN:fName,lastN:lName} = person1
+console.log(fName,lName);
+
+//default values
+
+const setting = {
+    theme:"vanilla"
+}
+
+//const {theme,defaultTheme:"Dark"} = setting; //SyntaxError: Invalid destructuring assignment target not : but =
+//const { propertyName = defaultValue } = object;
+const{theme,defaultTheme="Dark"} = setting;
+console.log(theme,defaultTheme);
 
